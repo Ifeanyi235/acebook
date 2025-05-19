8aa137e2c01033bf3e2e1cf36398c3c500d5330a
ifeanyi235 <ichuku2352gmail.com>
2025-05-19 20:41:10 +0100

    used app.options

--- server/server.js
+++ server/server.js
@@ -28,6 +28,8 @@ app.options('*', cors({
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "https://acebook-theta.vercel.app");
   res.header("Access-Control-Allow-Credentials", "true");
+  res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
 
